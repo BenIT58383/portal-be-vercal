63a1a26c14b17dd8fe110b3e75545e7e8b053eec
Ben <91747592+BenIT58383@users.noreply.github.com>
2023-03-21 13:41:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
-const cors = require("cors")
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
 const app = express();
-app.use(cors())
-require("dotenv").config();
-const port = process.env.PORT || 8000
-const {readdirSync} = require("fs")
 
+var corsOptions = {
+  // origin: "http://localhost:8081"
+  origin: "*"
+};
 
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+// simple route
 app.get("/", (req, res) => {
-  res.send("Main page!");
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
+require("./app/routes/tutorial.routes.js")(app);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
