3c727fb94af7f8e370133ef1014d63236965e530
DAM VAN BEN <bendv@techasians.com>
2023-03-21 14:39:54 +0700

    v

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  // origin: "http://localhost:8081"
-  origin: "*"
-};
+// var corsOptions = {
+//   // origin: "http://localhost:8081"
+//   origin: "*"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 // app.use(express.json()); /* bodyParser.json() is deprecated */
